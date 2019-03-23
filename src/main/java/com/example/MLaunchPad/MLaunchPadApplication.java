97612006f7a60f9db2317d630abee450b2e1f52e
Marski96 <jani.maaranen@saunalahti.fi>
2019-03-23 14:20:02 +0200

    Starting project

---
+++ src/main/java/com/example/MLaunchPad/MLaunchPadApplication.java
@@ -0,0 +1,13 @@
+package com.example.MLaunchPad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MLaunchPadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MLaunchPadApplication.class, args);
+	}
+
+}
