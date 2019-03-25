b55c1b7585ad9aa8d3a0d435d3e63781e8d43bc4
Marski96 <jani.maaranen@saunalahti.fi>
2019-03-25 22:22:49 +0200

    added delete and edit buttons

--- src/main/java/com/example/MLaunchPad/web/LaunchPadController.java
+++ src/main/java/com/example/MLaunchPad/web/LaunchPadController.java
@@ -3,7 +3,9 @@ package com.example.MLaunchPad.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.MLaunchPad.domain.TypeRepository;
 import com.example.MLaunchPad.domain.toDoRepository;
@@ -35,7 +37,21 @@ public class LaunchPadController {
 			model.addAttribute("toDos", toDoRepo.findAll());
 			return "Mhome";
 	}
-		
+	
+	//Delete toDo
+	@RequestMapping(value="/delete/{id}", method=RequestMethod.GET)
+		public String deleteToDo(@PathVariable("id") long id, Model model) {
+			toDoRepo.deleteById(id);
+			return "redirect:../Mhome";
+	}
+	
+	//Edit toDo
+	@RequestMapping(value="/edit/{id}")
+		public String edit(@PathVariable("id") long id, Model model) {
+			model.addAttribute("toDo", toDoRepo.findById(id));
+			model.addAttribute("type", typeRepo.findAll());
+			return "edit";
+	}
 	
 
 }
